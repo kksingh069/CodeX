214135735b421570cbaaa86c35d7151a4866f832
kksingh069 <86555130+kksingh069@users.noreply.github.com>
2023-04-05 22:25:49 +0530

    Create convert a string into upper-case or lower-case.cpp

---
+++ convert a string into upper-case or lower-case.cpp
@@ -0,0 +1,16 @@
+# include < iostream>
+# include < string>
+using namespace std;
+
+int main()
+{
+char str1 [30], temp[30];
+cout < < "Enter the string:";
+cin > > str1;
+strcpy ( temp, str1);
+cout < < "strupr ( temp ) : " < < strupr (temp) < < end1;
+cout < < "strlwr (temp) : " < < strlwr ( temp) < < end1;
+system("pause");
+
+return 0;
+}
