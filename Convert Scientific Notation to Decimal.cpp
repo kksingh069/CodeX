2e1bf30c46699cfe71a3bf63677ae4c38952eb50
kksingh069 <86555130+kksingh069@users.noreply.github.com>
2023-04-12 19:36:31 +0530

    Create Convert Scientific Notation to Decimal.cpp

---
+++ Convert Scientific Notation to Decimal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+  
+using namespace std;
+  
+int main()
+{
+    int n = 9;
+  
+    cout << "pow(2,9) : " << pow(2, 9) << endl;
+    cout << "pow(10,n) : " << pow(10, n) << endl;
+  
+    return 0;
+}
