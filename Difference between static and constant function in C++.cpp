15022079bf16550c183cc287e73c554d120c9fa4
kksingh069 <86555130+kksingh069@users.noreply.github.com>
2023-05-17 22:57:43 +0530

    Create Difference between static and constant function in C++.cpp

---
+++ Difference between static and constant function in C++.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+  
+
+class A {
+
+public:
+
+    static void f()
+
+    {
+
+        cout << "GeeksforGeeks!";
+
+    }
+
+};
+
+  
+
+
+int main()
+
+{
+
+    A::f();
+
+}
