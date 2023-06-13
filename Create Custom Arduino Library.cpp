d2b2fe86ce1e8e46409671e32cc5533413636327
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-06-13 23:33:30 +0530

    Create Create Custom Arduino Library.cpp

---
+++ Create Custom Arduino Library.cpp
@@ -0,0 +1,20 @@
+
+#ifndef MY_LIBRARY_H
+#define MY_LIBRARY_H
+#include <Arduino.h>
+  
+class DCMotor {
+private:
+    byte pin1;
+    byte pin2;
+    int speed;
+  
+public:
+    DCMotor(byte, byte, int);
+    void clockwise();
+    void antiClockwise();
+    void stop();
+    void motorDelay();
+};
+  
+#endif
