c5f048f1a38c7d8d238ec814489e410b2cf07459
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-06-18 22:55:39 +0530

    Create Calculate Running Time in Microseconds.cpp

---
+++ Calculate Running Time in Microseconds.cpp
@@ -0,0 +1,36 @@
+#include <chrono>
+#include <iostream>
+ 
+using namespace std;
+ 
+// this namespace is used
+// to perform calculation
+using namespace chrono;
+ 
+// A function performing 10^7 operations
+void function_performing_iterations()
+{
+    for (int i = 0; i < 10000000;) {
+        i++;
+    }
+}
+ 
+//  Driver Code
+int main()
+{
+    // Timer starts now
+    auto start1 = high_resolution_clock::now();
+ 
+    function_performing_iterations();
+ 
+    // Timer ends now
+    auto stop1 = high_resolution_clock::now();
+    // Difference is calculated
+    auto duration
+        = duration_cast<microseconds>(stop1 - start1);
+ 
+    // Displays time in output file in
+    // microseconds
+    cerr << "Time taken in microseconds : "
+         << (double)(duration.count() / 1000.0) << endl;
+}
