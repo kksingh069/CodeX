bb20274b6085e842e48ed0453af6313dc49d3b51
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-06-20 22:51:03 +0530

    Create Program to Create an Interface.cpp

---
+++ Program to Create an Interface.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Interface(Abstract class
+// with pure virtual function)
+class GFG
+{
+public:
+	virtual string returnString() = 0;
+};
+
+class child : public GFG
+{
+public:
+	string returnString()
+	{
+	return "GeeksforGeeks";
+	}
+};
+
+// Driver code
+int main()
+{
+	child childObj;
+	GFG* ptr;
+	ptr = &childObj;
+	cout << ptr->returnString();
+	return 0;
+}
