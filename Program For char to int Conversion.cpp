11d67826489759bd797bba46d7e0813af6782105
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-06-23 20:41:43 +0530

    Create Program For char to int Conversion.cpp

---
+++ Program For char to int Conversion.cpp
@@ -0,0 +1,12 @@
+// C++ program to convert
+// char to int (ASCII Value) using typecasting
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+	char ch = 'A';
+	cout << int(ch);
+	return 0;
+}
