a385c596cbe281bf8afb397547361b41c5d077a5
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-06-28 21:04:19 +0530

    Create How a statement is handled inside switch block but outside case.c

---
+++ How a statement is handled inside switch block but outside case.c
@@ -0,0 +1,15 @@
+// C program to illustrate the switch statement
+#include <stdio.h>
+
+// Driver Code
+int main()
+{
+	switch (1) {
+		int test = 10;
+		printf("dead code\n");
+	case 1:
+		printf("%d\n", test);
+	}
+
+	return 0;
+}
