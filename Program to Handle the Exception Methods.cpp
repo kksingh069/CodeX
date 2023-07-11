bee85fc3a1e4f22d380294dddfd5761f71f09da8
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-07-11 11:29:20 +0530

    Create Program to Handle the Exception Methods.cpp

---
+++ Program to Handle the Exception Methods.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+int numerator = 5;
+int denominator = 0;
+	
+int ans = numerator/denominator;
+	
+// The result will not be displayed
+cout << ans << endl;
+	
+return 0;
+	
+}
