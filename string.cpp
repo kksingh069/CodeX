21e1d4330a10b0ba37c6ed9db26900ad68ecc8cb
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-07-14 01:15:32 +0530

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,32 @@
+// C++ Program to demonstrate
+// Using ASCII value
+#include <iostream>
+using namespace std;
+
+void is_digits(string& str)
+{
+	for (char ch : str) {
+		int v = ch; // ASCII Val converted
+		if (!(ch >= 48 && ch <= 57)) {
+			cout << "False" << endl;
+			return;
+		}
+	}
+
+	cout << "True" << endl;
+}
+
+// Driver Code
+int main()
+{
+	string str = "125";
+	is_digits(str);
+
+	str = "1h34";
+	is_digits(str);
+
+	str = "012";
+	is_digits(str);
+
+	return 0;
+}
