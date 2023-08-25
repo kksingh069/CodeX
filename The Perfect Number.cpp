340942e81882010c472cf41bec57489c506c7e4f
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-08-25 22:40:49 +0530

    Create The Perfect Number.cpp

---
+++ The Perfect Number.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+void main() {
+	clrscr();
+	int i=1, u=1, sum=0;
+	while(i<=500) {
+		while(u<=500) {
+			if(u<i) {
+				if(i%u==0 )
+				      sum=sum+u;
+			}
+			u++;
+		}
+		if(sum==i) {
+			cout<<i<<" is a perfect number."<<"\n";
+		}
+		i++;
+		u=1;
+		sum=0;
+	}
+	getch();
+}
