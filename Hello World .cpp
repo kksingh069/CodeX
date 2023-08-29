1a9761b7276be9b7a9616aebb6b7d6c2a984cee1
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-08-29 19:27:31 +0530

    Create Hello World .cpp

---
+++ Hello World .cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+//Standard namespace declaration
+using namespace std;
+//Main Function
+int main() {
+	//Standard Ouput Statement
+	cout<<"My First C++ Program";
+	// Wait For Output Screen
+	getch();
+	//Main Function return Statement
+	return 0;
+}
