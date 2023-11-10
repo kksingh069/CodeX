be6f4ea21f7274b9b7ff1a038a0a04f463a63e38
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-11-10 23:29:34 +0530

    Create Ellipsis in C++ with Examples.cpp

---
+++ Ellipsis in C++ with Examples.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// Driver Code 
+int main() 
+{ 
+    // Below Line will given Compilation 
+    // Error as 4 arguments are passed 
+    // instead of 2 
+    int l = max(4, 5, 6, 7); 
+    cout << l; 
+    return 0; 
+} 
