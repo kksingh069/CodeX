4aa979bf57cd136138b2b086bc8fccbb29fad605
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-12-03 00:50:09 +0530

    Update Passing and Returning Objects.cpp

--- Passing and Returning Objects.cpp
+++ Passing and Returning Objects.cpp
@@ -4,21 +4,14 @@ using namespace std;
 class Example {
 public:
 	int a;
-
-	void add(Example E)
+        void add(Example E)
 	{
 		a = a + E.a;
 	}
 };
-
-// Driver Code
 int main()
 {
-
-	// Create objects
 	Example E1, E2;
-
-	// Values are initialized for both objects
 	E1.a = 50;
 	E2.a = 100;
 
@@ -26,13 +19,8 @@ int main()
 	cout << "Value of object 1: " << E1.a
 		<< "\n& object 2: " << E2.a
 		<< "\n\n";
-
-	// Passing object as an argument
-	// to function add()
 	E2.add(E1);
 
-	// Changed values after passing
-	// object as argument
 	cout << "New values \n";
 	cout << "Value of object 1: " << E1.a
 		<< "\n& object 2: " << E2.a
