7cb71d61af1cdd0eba3c921a855876770992be9d
Krishna Kant Singh <86555130+kksingh069@users.noreply.github.com>
2023-12-25 23:53:20 +0530

    Create program to read file word by word.cpp

---
+++ program to read file word by word.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// driver code
+int main()
+{
+    // filestream variable file
+    fstream file;
+    string word, t, q, filename;
+ 
+    // filename of the file
+    filename = "file.txt";
+ 
+    // opening file
+    file.open(filename.c_str());
+ 
+    // extracting words from the file
+    while (file >> word)
+    {
+        // displaying content
+        cout << word << endl;
+    }
+ 
+    return 0;
+}
